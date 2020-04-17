b2b11196e739afd224e4af843ffc5503242fa68e
Asha-art <62064699+Asha-art@users.noreply.github.com>
2020-04-16 20:21:28 -0400

    add files

--- post.js
+++ post.js
@@ -17,20 +17,18 @@ $('#all').click(function(){
 $('#post10').click(function(){
 	$.get('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts',{id:10}, function(result){
 		console.log(result);
-	//list.empty();
 		result.forEach(function(){
 			var li = $('<li></li>');
     li.text(JSON.stringify(result));
     $('body').append(li);
-   // li.text(x);
-			//list.append(li);
+   
 		})
   })
 }) 
 
 //3
-$('#comment12').click(function(){
-	$.get('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/comments',{id:12}, function(result){
+$('#comment14').click(function(){
+	$.get('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/comments',{id:14}, function(result){
 		console.log(result);
 	//list.empty();
 		result.forEach(function(){
@@ -58,34 +56,91 @@ $('#post2').click(function(){
 
 //Create new Post
 $('#newPost').click(function(){
-  var newUserPost= "New Post";
-	$.post('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts', {title:newUserPost},function(result){
-
-    console.log(result);
-
-  })
-}) 
+	$.post('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts', {
+		userID: 45,
+		id: 18,
+		title: "create new post",
+		body: "Lorem ipsum"
+	} , function(result){
+		console.log(result);
+			var li = $('<li></li>');
+			li.text(JSON.stringify(result));
+			$('body').append(li);
+		})
+})
 
 //Replace the post
 
-$('#replace12').click(function(){
-	$.post('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts',{id:12}, function(result){
+$('#replace14').click(function(){
+	$.ajax({
+		method: 'PUT',
+		url: 'https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts/14',
+		data: { title: "post replaced"},
+		 function(response){
+			console.log(response.responseJSON);//handle response which usually includes the updated object.
+		}
+	},
+	function(result){
 		console.log(result);
-			result.forEach(function(){
 			var li = $('<li></li>');
-    li.text(JSON.stringify(result));
-    $('body').append(li);
-  
+			li.text(JSON.stringify(result));
+			$('body').append(li);
 		})
   })
-}) 
+
 
 //Update the title of post 
+$('#update14').click(function(){
+	$.post('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts/14',{
+	id :14,	title: "post updated"},
+		function(response){
+			console.log(response.responseJSON);//Handle response, which usually contains the updated object including new ID's
 
-// Delete the post with id 12
+		})
+	},
+	function(result){
+		console.log(result);
+			var li = $('<li></li>');
+			li.text(JSON.stringify(result));
+			$('body').append(li);
+		})
+  
+// Delete the post with id 14 ...Remove the object
+$('#update14').click(function(){
+$.ajax({
+	method: 'DELETE',
+	url: 'https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts/14',
+	complete: function(response){
+		console.log(response.responseJSON);
+		}
+	},function(result){
+		console.log(result);
+		var li = $('<li></li>');
+		li.text(JSON.stringify(result));
+		$('body').append(li);
+	})
+})
+
 
 // display a list of posts
+$('#displayAll').click(function(){
+	$.get('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts', function(users){
+		//Iterate over the response, adding elements to DOM
+		users.forEach(function(){
+			console.log(users);
+			var li = $('<li></li>');
+			li.text(JSON.stringify(users));
+		$('body').append(li);
+		})
+	})
+})
 
 // display all comments when user click on a post
+$('#commentPost').click(function(){
+	
 
+})
 //display a link back to all posts
+$('#linkPost').click(function(){
+	
+})
